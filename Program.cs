578f0618bafca7fd5b27aba62521210c2efbf67c
Prof. Cristiano de Paula <cristiano.paula@docente.senai.br>
2025-07-31 10:33:32 -0300

    Adicionar arquivos de projeto.

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿internal class Program
+{
+    private static void Main(string[] args)
+    {
+        var numero1 = 2;
+        var numero2 = 3;
+
+        var soma = numero1 + numero2;
+
+        Console.WriteLine("Bem vindo ao Mundo de Programação C#");
+        Console.WriteLine("Olá, Prof. Cristiano!");
+
+        Console.WriteLine("A soma é igual a: " + soma);
+    }
+}
